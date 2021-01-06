5b55689be16eb59fbdf86783ddb42862e6865a7b
vova-lantsov-dev <vova.lantsov.dev@gmail.com>
2021-01-06 23:06:38 +0200

    #4 prepare Build.cs for TeamCity hosting

--- build/Build.cs
+++ build/Build.cs
@@ -1,20 +1,17 @@
-using System;
-using System.Linq;
 using Nuke.Common;
+using Nuke.Common.CI;
+using Nuke.Common.CI.TeamCity;
 using Nuke.Common.Execution;
-using Nuke.Common.Git;
 using Nuke.Common.IO;
-using Nuke.Common.ProjectModel;
 using Nuke.Common.Tooling;
-using Nuke.Common.Tools.DotNet;
-using Nuke.Common.Utilities.Collections;
-using static Nuke.Common.EnvironmentInfo;
-using static Nuke.Common.IO.FileSystemTasks;
-using static Nuke.Common.IO.PathConstruction;
-using static Nuke.Common.Tools.DotNet.DotNetTasks;
 
 [CheckBuildProjectConfigurations]
 [ShutdownDotNetAfterServerBuild]
+[TeamCity(
+    TeamCityAgentPlatform.Unix,
+    Version = "2020.1",
+    VcsTriggeredTargets = new[] {nameof(Up)},
+    ManuallyTriggeredTargets = new[] {nameof(Up), nameof(Down)})]
 class Build : NukeBuild
 {
     /// Support plugins are available for:
@@ -23,42 +20,37 @@ class Build : NukeBuild
     ///   - Microsoft VisualStudio     https://nuke.build/visualstudio
     ///   - Microsoft VSCode           https://nuke.build/vscode
 
-    public static int Main () => Execute<Build>(x => x.Compile);
+    public static int Main () => Execute<Build>(x => x.Up);
 
-    [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
-    readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
+    /*[Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
+    readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;*/
 
-    [Solution] readonly Solution Solution;
-    [GitRepository] readonly GitRepository GitRepository;
+    [Parameter("Check both checkboxes to wipe the database data")]
+    readonly bool WipeDatabaseData;
+
+    [Parameter(Name = "DATABASE_WIPE_SHIELD_CHECKBOX")]
+    readonly bool WipeDatabaseDataShield;
+
+    //[Solution] readonly Solution Solution;
+    [PathExecutable("docker-compose")] readonly Tool DockerCompose;
 
     AbsolutePath SourceDirectory => RootDirectory / "src";
-    AbsolutePath TestsDirectory => RootDirectory / "tests";
-    AbsolutePath OutputDirectory => RootDirectory / "output";
+    //AbsolutePath TestsDirectory => RootDirectory / "tests";
+    //AbsolutePath OutputDirectory => RootDirectory / "output";
 
-    Target Clean => _ => _
-        .Before(Restore)
+    Target Down => _ => _
         .Executes(() =>
         {
-            SourceDirectory.GlobDirectories("**/bin", "**/obj").ForEach(DeleteDirectory);
-            TestsDirectory.GlobDirectories("**/bin", "**/obj").ForEach(DeleteDirectory);
-            EnsureCleanDirectory(OutputDirectory);
+            if (WipeDatabaseData && WipeDatabaseDataShield)
+                DockerCompose("down --volumes", SourceDirectory);
+            else
+                DockerCompose("down", SourceDirectory);
         });
 
-    Target Restore => _ => _
+    Target Up => _ => _
+        .After(Down)
         .Executes(() =>
         {
-            DotNetRestore(s => s
-                .SetProjectFile(Solution));
+            DockerCompose("up --build -d", SourceDirectory);
         });
-
-    Target Compile => _ => _
-        .DependsOn(Restore)
-        .Executes(() =>
-        {
-            DotNetBuild(s => s
-                .SetProjectFile(Solution)
-                .SetConfiguration(Configuration)
-                .EnableNoRestore());
-        });
-
 }
