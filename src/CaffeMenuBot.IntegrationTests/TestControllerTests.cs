c629722b92b678ae4fd5ff02e1460f03e917616f
vova-lantsov-dev <vova.lantsov.dev@gmail.com>
2021-01-08 00:34:22 +0200

    #7 add integration tests project

---
+++ src/CaffeMenuBot.IntegrationTests/TestControllerTests.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading.Tasks;
+using CaffeMenuBot.AppHost;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Xunit;
+
+namespace CaffeMenuBot.IntegrationTests
+{
+    public sealed class TestControllerTests
+        : IClassFixture<WebApplicationFactory<Startup>>
+    {
+        private readonly WebApplicationFactory<Startup> _factory;
+
+        public TestControllerTests(WebApplicationFactory<Startup> factory)
+        {
+            _factory = factory;
+        }
+
+        [Fact]
+        public async Task Get_TestEndpoint()
+        {
+            var client = _factory.CreateClient();
+            var response = await client.GetStringAsync("api/test");
+            Assert.Equal("OK", response);
+        }
+    }
+}
