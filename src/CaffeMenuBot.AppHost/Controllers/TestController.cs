0c84e9efd733ffe1b34f6c2f3038dd92590a042e
vova-lantsov-dev <vova.lantsov.dev@gmail.com>
2021-01-07 10:39:23 +0200

    #1 added test controller

---
+++ src/CaffeMenuBot.AppHost/Controllers/TestController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CaffeMenuBot.AppHost.Controllers
+{
+    [ApiController]
+    [Route("api/test")]
+    public sealed class TestController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult Test()
+        {
+            return Content("OK");
+        }
+    }
+}
