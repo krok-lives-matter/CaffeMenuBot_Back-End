150ae5617d88659759a049321cb97543ca468f03
Edvard Potapenko <nothingselfless@gmail.com>
2021-01-13 14:40:57 +0200

    #8 added roles

---
+++ src/CaffeMenuBot.AppHost/Authentication/Roles.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Authorization;
+
+namespace CaffeMenuBot.AppHost.Authentication
+{
+    internal static class Roles
+    {
+        public const string Admin = "admin";
+        public static AuthorizationPolicy AdminPolicy() =>
+            new AuthorizationPolicyBuilder().RequireAuthenticatedUser().RequireRole(Admin).Build();
+    }
+}
