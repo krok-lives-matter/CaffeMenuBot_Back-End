5c9b25f131c4393769b1342103a46265361dbaa8
Edvard Potapenko <nothingselfless@gmail.com>
2021-01-16 18:29:56 +0200

    #9 added dashboard and mock dashboard repos

---
+++ src/CaffeMenuBot.Data/Contracts/IDashboardRepo.cs
@@ -0,0 +1,17 @@
+﻿using CaffeMenuBot.Data.Models.Menu;
+using System.Collections.Generic;
+
+namespace CaffeMenuBot.Data.Contracts
+{
+    public interface IDashboardRepo
+    {
+        public bool SaveChanges();
+        public IEnumerable<Dish>? GetAllDishes();
+        public Dish? GetDishById(int id);
+        public void AddDish(Dish dish);
+
+        public IEnumerable<Category>? GetAllCategories();
+        public Category? GetCategoryById(int id);
+        public void AddCategory(Category category);
+    }
+}
