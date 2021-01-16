b98e2b93f88bb5e2089cf0b85d612e45f1793390
Edvard Potapenko <nothingselfless@gmail.com>
2021-01-16 18:32:24 +0200

    #9 added subcategories migrations

---
+++ src/CaffeMenuBot.Data/Migrations/20210116153039_SubCategories.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CaffeMenuBot.Data.Migrations
+{
+    public partial class SubCategories : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ParentCategoryId",
+                schema: "public",
+                table: "categories",
+                type: "integer",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_categories_ParentCategoryId",
+                schema: "public",
+                table: "categories",
+                column: "ParentCategoryId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_categories_categories_ParentCategoryId",
+                schema: "public",
+                table: "categories",
+                column: "ParentCategoryId",
+                principalSchema: "public",
+                principalTable: "categories",
+                principalColumn: "category_id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_categories_categories_ParentCategoryId",
+                schema: "public",
+                table: "categories");
+
+            migrationBuilder.DropIndex(
+                name: "IX_categories_ParentCategoryId",
+                schema: "public",
+                table: "categories");
+
+            migrationBuilder.DropColumn(
+                name: "ParentCategoryId",
+                schema: "public",
+                table: "categories");
+        }
+    }
+}
