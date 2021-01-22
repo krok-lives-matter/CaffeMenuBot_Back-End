4366f5a1c5b8f61788f024636d599c107734fe8d
Edvard Potapenko <you@example.com>
2021-01-22 23:28:46 +0200

    #9 added update and delete by id for categories and dishes

--- src/CaffeMenuBot.AppHost/Controllers/DashboardApiController.cs
+++ src/CaffeMenuBot.AppHost/Controllers/DashboardApiController.cs
@@ -23,6 +23,8 @@ namespace CaffeMenuBot.AppHost.Controllers
             _context = context;
         }
 
+        #region dishes
+
         //GET api/dashboard/getAllDishes
         [HttpGet("getAllDishes")]
         public async Task<ActionResult<IEnumerable<Dish>>> GetAllDishes(CancellationToken cancellationToken)
@@ -50,6 +52,26 @@ namespace CaffeMenuBot.AppHost.Controllers
             return dish;
         }
 
+        //GET api/dashboard/deleteDish/{id}
+        [HttpGet("deleteDish/{id:int:min(1)}")]
+        public async Task<ActionResult<Dish>> DeleteDishById(int id, CancellationToken cancellationToken)
+        {
+            var dishToDelete = await _context
+                .Dishes
+                .AsNoTracking()
+                .FirstOrDefaultAsync(d => d.Id == id, cancellationToken);
+
+            if (dishToDelete == null)
+                return NotFound();
+
+            _context.Remove(dishToDelete);
+
+            await _context.SaveChangesAsync();
+
+            return Ok();
+        }
+
+        //POST api/dashboard/addDish
         [HttpPost("addDish")]
         public async Task<ActionResult<CreatedItemResult>> AddDish([FromBody] Dish dish, CancellationToken cancellationToken)
         {
@@ -59,6 +81,20 @@ namespace CaffeMenuBot.AppHost.Controllers
 
             return new CreatedItemResult {CreatedItemId = dish.Id};
         }
+
+        //POST api/dashboard/updateDish
+        [HttpPost("updateDish")]
+        public async Task<ActionResult<CreatedItemResult>> UpdateDish([FromBody] Dish dish, CancellationToken cancellationToken)
+        {
+            _context.Dishes.Update(dish);
+            await _context.SaveChangesAsync(cancellationToken);
+
+            return new CreatedItemResult { CreatedItemId = dish.Id };
+        }
+
+        #endregion
+
+        #region categories
 
         //GET api/dashboard/getAllCategories
         [HttpGet("getAllCategories")]
@@ -83,6 +119,38 @@ namespace CaffeMenuBot.AppHost.Controllers
             
             return category;
         }
+        
+
+        // WARNING deletes all related subcategories and dishes
+        //GET api/dashboard/deleteCategory/{id}
+        [HttpGet("deleteCategory/{id:int:min(1)}")]
+        public async Task<ActionResult<Category>> DeleteCategoryById(int id, CancellationToken cancellationToken)
+        {
+            var categoryToDelete = await _context
+                .Categories
+                .AsNoTracking()
+                .FirstOrDefaultAsync(c => c.Id == id, cancellationToken);
+
+            if (categoryToDelete == null)
+                return NotFound();
+
+            if(categoryToDelete.ParentCategoryId == null)
+            {
+                var subcategories = await _context.GetAllCategoriesRecursivelyAsync(cancellationToken, categoryToDelete.Id);
+
+                if (subcategories == null)
+                    return Problem("failed to delete subcategories", "subcategories", 500, "Delete operation failed", "error");
+
+                _context.RemoveRange(subcategories);
+            }
+
+            _context.Remove(categoryToDelete);
+
+            await _context.SaveChangesAsync();
+
+            return Ok();
+        }
+
 
         //POST api/dashboard/addCategory
         [HttpPost("addCategory")]
@@ -95,5 +163,19 @@ namespace CaffeMenuBot.AppHost.Controllers
 
             return new CreatedItemResult {CreatedItemId = category.Id};
         }
+
+        //POST api/dashboard/updateCategory
+        [HttpPost("updateCategory")]
+        public async Task<ActionResult<CreatedItemResult>> UpdateCategory([FromBody] Category category,
+            CancellationToken cancellationToken)
+        {
+            // ReSharper disable once MethodHasAsyncOverloadWithCancellation
+            _context.Categories.Update(category);
+            await _context.SaveChangesAsync(cancellationToken);
+
+            return new CreatedItemResult { CreatedItemId = category.Id };
+        }
+
+        #endregion
     }
 }
