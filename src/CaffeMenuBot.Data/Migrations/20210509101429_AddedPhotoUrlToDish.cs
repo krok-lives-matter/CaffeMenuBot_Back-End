08c96963cd4aad6be48ac2f26d905d7ee7949007
Edvard Potapenko <nothingselfless@gmail.com>
2021-05-09 13:15:22 +0300

    removed loginModel - not needed, added photo url to dish

---
+++ src/CaffeMenuBot.Data/Migrations/20210509101429_AddedPhotoUrlToDish.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CaffeMenuBot.Data.Migrations
+{
+    public partial class AddedPhotoUrlToDish : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "photo_url",
+                schema: "public",
+                table: "dishes",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "photo_url",
+                schema: "public",
+                table: "dishes");
+        }
+    }
+}
