a3a0785c90c6e6717f2c1c6a4d3f26cbf00bd8ab
Edvard Potapenko <nothingselfless@gmail.com>
2021-05-09 19:27:29 +0300

    added schedule command; removed legacy config; removed unnecesarry usings

--- src/CaffeMenuBot.Bot/Services/CommandPatternManager.cs
+++ src/CaffeMenuBot.Bot/Services/CommandPatternManager.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Telegram.Bot.Types;
