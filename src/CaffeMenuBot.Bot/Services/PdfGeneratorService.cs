cea4d40ec0a17ef137e0f1dd5474d3bd02640d74
vova-lantsov-dev <vova.lantsov.dev@gmail.com>
2021-05-30 20:20:04 +0300

    Migrate from PDFFlowLib to iText7

--- src/CaffeMenuBot.Bot/Services/PdfGeneratorService.cs
+++ src/CaffeMenuBot.Bot/Services/PdfGeneratorService.cs
@@ -3,8 +3,12 @@ using System.Reflection;
 using System.Threading;
 using System.Threading.Tasks;
 using CaffeMenuBot.Data.Models.Menu;
-using Gehtsoft.PDFFlow.Builder;
-using Gehtsoft.PDFFlow.Models.Enumerations;
+using iText.IO.Image;
+using iText.Kernel.Geom;
+using iText.Kernel.Pdf;
+using iText.Layout;
+using iText.Layout.Element;
+using Path = System.IO.Path;
 
 namespace CaffeMenuBot.Bot.Services
 {
@@ -17,27 +21,28 @@ namespace CaffeMenuBot.Bot.Services
     {
         public ValueTask<Stream> GenerateMenuForCategoryAsync(Category category, CancellationToken cancellationToken)
         {
-            var document = DocumentBuilder.New();
-            var section = document.AddSection()
-                .SetMargins(0f)
-                .SetSize(PaperSize.A4)
-                .SetOrientation(PageOrientation.Portrait);
-            
-            AddHeaderImage(section);
-
             var memoryStream = new MemoryStream();
-            document.Build(memoryStream);
+            var pdfWriter = new PdfWriter(memoryStream);
+            var pdfDocument = new PdfDocument(pdfWriter);
+            pdfDocument.SetDefaultPageSize(PageSize.A4);
+            var document = new Document(pdfDocument);
+            
+            document.SetMargins(0f, 0f, 0f, 0f);
+            
+            AddHeaderImage(document);
+            
+            document.Close();
             memoryStream.Position = 0L;
             return new ValueTask<Stream>(memoryStream);
         }
 
-        private static void AddHeaderImage(SectionBuilder section)
+        private static void AddHeaderImage(Document document)
         {
             const string headerImageFileName = "header.png";
             var currentDir = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
             string fullPathToHeaderImage = Path.Combine(currentDir!, "Content", "images", headerImageFileName);
-
-            section.AddImage(fullPathToHeaderImage, ScalingMode.Stretch);
+            
+            document.Add(new Image(ImageDataFactory.Create(fullPathToHeaderImage)));
         }
     }
 }
